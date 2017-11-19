6c40ef2ee0854edf3af7ea5d8b03fee3f0397027
NoellePatterson <32949245+NoellePatterson@users.noreply.github.com>
2017-11-19 14:34:24 -0800

    Marginal improvements in readability

--- dimensionless-hydrograph/ConvertToWaterYearMatrix.R
+++ dimensionless-hydrograph/ConvertToWaterYearMatrix.R
@@ -2,14 +2,14 @@ ConvertToWaterYearMatrix <- function(QData, month, day) {
   indexOfWaterYears <- numeric()
   numRows <- length(QData[,1])
   
+  #Create new column in matrix each time start month and day are repeated in the timeseries
   for (rowIndex in 1:numRows) {
-    mo <- as.numeric(strsplit(QData[rowIndex,1], "-")[[1]][2])
-    dy <- as.numeric(strsplit(QData[rowIndex,1], "-")[[1]][3])
+    mo <- as.numeric(strsplit(QData[rowIndex,1], "-")[[1]][2]) #identify all QData records with user-defined start month
+    dy <- as.numeric(strsplit(QData[rowIndex,1], "-")[[1]][3]) #identify all QData records with user-defined start day
     if (mo == month & dy == day){
-      indexOfWaterYears <- c(indexOfWaterYears, rowIndex)
+      indexOfWaterYears <- c(indexOfWaterYears, rowIndex) 
     } 
   }
-  
   return(indexOfWaterYears)
 }
 
