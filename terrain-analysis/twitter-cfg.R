ff1f0c7532db706aef8dcca33c735c9470bb2a1c
hrvg <33336471+hrvg@users.noreply.github.com>
2017-12-01 16:21:57 -0800

    Commented and curated code

---
+++ terrain-analysis/twitter-cfg.R
@@ -0,0 +1,5 @@
+# DO NOT SHARE THIS FILE
+consumer_key <- 'GzJaB65NiODVkOSZmf6BevLm1'
+consumer_secret <- "QuN6fERjF1whO9flEzk6InBfPx6euW8zwUpwqJ9cyzMES1y3Sq"
+access_token <- "932742926702059520-vbPGPVhRwiVAruTtRUMzRJ1cYkCnIZI"
+access_secret <- "86ta92HsRwFJRxUEbY4ufIhUmjeNoGio6OHSqx84xuuFN"
