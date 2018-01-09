5ddc58dd2e1df1ba6aa016434fde8db9163e7615
NoellePatterson <32949245+NoellePatterson@users.noreply.github.com>
2018-01-08 17:32:24 -0800

    Add files via upload
    Added capabilities for removal of trailing and internal NA's

--- dimensionless-hydrograph/dim-hydrograph-single/DimHydrographSingle.R
+++ dimensionless-hydrograph/dim-hydrograph-single/DimHydrographSingle.R
@@ -1,13 +1,14 @@
-#This code generates a dimensionless hydrograph plot from flow timeseries data, with user-defined water year
-#Developed by Noelle Patterson, UC Davis Water Management Lab, 2017
+# This code generates a dimensionless hydrograph plot from flow timeseries data, with user-defined water year
+# Developed by Noelle Patterson, UC Davis Water Management Lab, 2017
 
 rm(list = ls())
+library("zoo")
 
-#Define working directory and file where timeseries flow data is located
+# Define working directory and file where timeseries flow data is located
 workingDir <- getwd()
 inputFile <- "DimHydrograph_TestData.csv"
 
-#Define water year by two-digit month and day
+# Define water year by two-digit month and day
 month <- 10
 day <- 01
 
@@ -16,35 +17,37 @@ inputData <- read.csv(inputFile, header=TRUE, check.names = FALSE, na.strings = 
 
 QData <- inputData[ , c(1,2)] 
 
-QData <- na.omit(QData)
-
 colnames(QData) <- c("date", "Q")
-QData$date <- as.Date(QData$date, "%m/%d/%Y") #Make sure date column is in date format
-QData$Q <- as.double(QData$Q)  #Make sure flow column is in number format
+QData$Q <- as.numeric(as.character(QData$Q))
+QData <- na.trim(QData, sides = c("both")) # Remove leading and trailing NA's 
+QData$Q <- na.approx(QData$Q) # Replace NA's in data with interpolated values 
+
+QData$date <- as.Date(QData$date, "%m/%d/%Y") # Make sure date column is in date format
+QData$Q <- as.double(QData$Q)  # Make sure flow column is in number format
 QData <- as.matrix(QData)
 
 source("DefineDateIndices.R")
-list <- DefineDateIndices(QData, month, day) # input two digit month and day into function
-dateIndices <- unlist(list[1]) #Extract date indices from list
+list <- DefineDateIndices(QData, month, day) # Input two digit month and day into function
+dateIndices <- unlist(list[1]) # Extract date indices from list
 dateVector <- unlist(list[2]) # Extract julian date vector from list
 
 source("CreateFlowMatrixUsingDateIndex.R")
 Qmatrix <- CreateFlowMatrixUsingDateIndex(dateIndices, QData)
 
-#Find average annual flow for each water year in record
+# Find average annual flow for each water year in record
 Qmean <- rep(NA, ncol(Qmatrix))
 for(i in 1:ncol(Qmatrix)) {
   Qmean[i] <- mean(as.numeric(Qmatrix[,i]), na.rm = TRUE)
 }
 
-#Divide each daily flow value by avg annual daily flow. Save in a new data frame.
+# Divide each daily flow value by avg annual daily flow. Save in a new data frame.
 QmatrixNorm <- matrix(NA, nrow = 366, ncol = ncol(Qmatrix))
 QmatrixNorm <- as.data.frame(QmatrixNorm)
 for (i in 1:ncol(Qmatrix)) {
   QmatrixNorm[,i] <- as.numeric(Qmatrix[,i])/Qmean[i]
 }
 
-#Calculate percentiles and annual max/min for every day of the year (across all years)
+# Calculate percentiles and annual max/min for every day of the year (across all years)
 rowLength <- length(QmatrixNorm[,1])
 Qstats <- data.frame(rep(NA, rowLength))
 
@@ -58,7 +61,7 @@ for(i in 1:rowLength)  {
   Qstats[i,7] <-min(QmatrixNorm[i,], na.rm = TRUE)
 }
 
-#Add a column with Julian day 
+# Add a column with Julian day 
 Qstats <- cbind(dateVector, Qstats)
 colnames(Qstats) <- c("julianDay", "90%", "75%", "50%", "25%", "10%", "Max", "Min")
 
@@ -66,35 +69,35 @@ colnames(Qstats) <- c("julianDay", "90%", "75%", "50%", "25%", "10%", "Max", "Mi
 
 xaxis <- c(1:rowLength)
 
-ymax <- max(Qstats[,7] + 1) #Set y-axis limit above max flow value
-#ymax <- max(Qstats[,1] + 1) #Alternatively set y-axis limit above 90% flow, if not plotting max flow line 
+ymax <- max(Qstats[,7] + 1) # Set y-axis limit above max flow value
+#ymax <- max(Qstats[,1] + 1) # Alternatively set y-axis limit above 90% flow, if not plotting max flow line 
 
-#Create dimensionless hydrograph plot
+# Create dimensionless hydrograph plot
 plot(xaxis, Qstats[,2], type = "l", col = "navy", lwd = 2, xlab = "Julian Date", 
      ylab = "Daily streamflow/Average Annual Streamflow", xlim = c(0,366), ylim = c(0,ymax), xaxt = "n")
 title(main = "Dimensionless Hydrograph")
 grid(NA, NULL, lty = "solid", lwd = 1)
-x <- pretty(xaxis, 12) #generate well-spaced tick marks for x-axis
-x <- c(1, x[!x %in% c(0)]) #Replace tick mark at x-axis=0 with a tick at x-axis=1 
-axis(side = 1, at = x, labels = dateVector[x]) #Label x-axis tick marks with corresponding julian date
+x <- pretty(xaxis, 12) # Generate well-spaced tick marks for x-axis
+x <- c(1, x[!x %in% c(0)]) # Replace tick mark at x-axis=0 with a tick at x-axis=1 
+axis(side = 1, at = x, labels = dateVector[x]) # Label x-axis tick marks with corresponding julian date
 legend("topright", legend = c("0.90", "0.75", "0.50", "0.25", "0.10", "max/min"), lty = 1, 
        lwd = 2.5, col = c("navy", "royalblue2", "red", "royalblue2", "navy", "black"), inset = .02)
 
 
-#Add fill color to plot
+# Add fill color to plot
 polygon(c(xaxis, rev(xaxis)), c(Qstats[,3], rev(Qstats[,2])), col="lightsteelblue1", border = NA)
 polygon(c(xaxis, rev(xaxis)), c(Qstats[,4], rev(Qstats[,3])), col="lightsteelblue1", border = NA)
 polygon(c(xaxis, rev(xaxis)), c(Qstats[,5], rev(Qstats[,4])), col="lightsteelblue1", border = NA)
 polygon(c(xaxis, rev(xaxis)), c(Qstats[,6], rev(Qstats[,5])), col="lightsteelblue1", border = NA)
 
-#Add percentile lines on top of the fill color
+# Add percentile lines on top of the fill color
 lines(xaxis, Qstats[,2], type = "l", col = "navy", lwd = 2)
 lines(xaxis, Qstats[,3], type = "l", col = "royalblue2", lwd = 1.5)
 lines(xaxis, Qstats[,4], type = "l", col = "red", lwd = 2)
 lines(xaxis, Qstats[,5], type = "l", col = "royalblue2", lwd = 1.5)
 lines(xaxis, Qstats[,6], type = "l", col = "navy", lwd = 2)
-lines(xaxis, Qstats[,7], type = "l", col = "black", lwd = 1.5) #Comment out line if not plotting max flow values
-lines(xaxis, Qstats[,8], type = "l", col = "black", lwd = 1.5) #Comment out line if not plotting min flow values
+lines(xaxis, Qstats[,7], type = "l", col = "black", lwd = 1.5) # Comment out line if not plotting max flow values
+lines(xaxis, Qstats[,8], type = "l", col = "black", lwd = 1.5) # Comment out line if not plotting min flow values
 
 
 
